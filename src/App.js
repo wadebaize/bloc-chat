9c25a71d6128bdefc4936bdf1b3c04ea55978c0a
wadebaize <wadebaize@Wades-MBP.jetpack>
2018-06-20 23:39:12 -0600

    finished checkpoint Roomlist

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import * as firebase from 'firebase';
+import RoomList from './components/RoomList';
+
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyDT-6BLFOJK4RCaWV1ndMqWYRM2oY-8vD8",
+    authDomain: "bloc-chat-2000.firebaseapp.com",
+    databaseURL: "https://bloc-chat-2000.firebaseio.com",
+    projectId: "bloc-chat-2000",
+    storageBucket: "bloc-chat-2000.appspot.com",
+    messagingSenderId: "328649239902"
+  };
+  firebase.initializeApp(config);
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+        <header className= 'App-Header'>
+        <h1>Welcome to Bloc Chat</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <RoomList firebase={firebase} />
       </div>
     );
   }
---
+++ src/components/RoomList.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+
+class RoomList extends Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+      rooms: [],
+    };
+
+    this.roomsRef = this.props.firebase.database().ref('rooms');
+  }
+
+
+  componentDidMount() {
+   this.roomsRef.on('child_added', snapshot => {
+     const room = snapshot.val();
+     room.key = snapshot.key;
+     this.setState({ rooms: this.state.rooms.concat( room ) });
+   });
+ }
+
+ render() {
+     return (
+       <div>
+ 	       {this.state.rooms.map ((room,index) =>
+ 	         <li key={index}>
+ 	           {room.name}
+ 	         </li>
+            )
+          }
+        </div>
+     );
+   }
+ }
+
+
+export default RoomList;
