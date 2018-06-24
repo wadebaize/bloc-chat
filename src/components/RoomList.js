b1f299d20c232bf9f1416f77b933ce7751423b46
wadebaize <wadebaize@Wades-MBP.jetpack>
2018-06-24 12:33:45 -0600

    finished create-rooms

--- src/components/RoomList.js
+++ src/components/RoomList.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 
-class RoomList extends Component{
+class RoomList extends Component {
   constructor(props) {
     super(props);
     this.state = {
       rooms: [],
+      newRoomName: ''
     };
 
     this.roomsRef = this.props.firebase.database().ref('rooms');
@@ -19,19 +20,50 @@ class RoomList extends Component{
    });
  }
 
- render () {
-     return (
-       <div>
- 	       {this.state.rooms.map ((room,index) =>
- 	         <li key={index}>
- 	           {room.name}
- 	         </li>
-            )
-          }
-        </div>
-     );
+ handleChange(e) {
+  this.setState({newRoomName: e.target.value});
+}
+
+ handleRoomSubmit(e){
+   e.preventDefault();
+   if (this.state.newRoomName) {
+     this.setState({newRoomName: ''});
+     this.roomsRef.push({name: this.state.newRoomName,});
    }
  }
 
+ render () {
+     return (
+       <section>
+         <section>
+
+           <form onSubmit={ (e) => this.handleRoomSubmit(e)}>
+              <label>Create New Room </label>
+              <input
+                type="text"
+                value={this.state.newRoomName}
+                onChange={(e) => this.handleChange(e)}
+                />
+              <input
+                type="submit"
+                value="submit"
+                />
+           </form>
+
+         </section>
+         <section>
+
+           {this.state.rooms.map ((room, index) =>
+             <li key={index}>
+               {room.name}
+             </li>
+             )
+           }
+
+         </section>
+       </section>
+    );
+  }
+}
 
 export default RoomList;
